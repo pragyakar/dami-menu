bf3f6662f422973927f3bdff5b1627cd488b6bfd
pragyakar <pragyakar75@gmail.com>
2019-03-16 21:28:20 +0545

    :sparkles: Added Landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => ( 
-  <h1>Landing</h1>
+  <div className="landing">
+    <p className="landing-title">Dami Menu</p>
+    <Link to='/menu' className="landing-button">Check it out</Link>
+  </div>
 );
 
 export default Landing;
