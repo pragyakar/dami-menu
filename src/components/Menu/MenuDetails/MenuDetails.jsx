c97fcd55d36a6426a739e591f3a8a8824a609b86
pragyakar <pragyakar75@gmail.com>
2019-03-16 23:17:36 +0545

    :sparkles: Added back link

--- src/components/Menu/MenuDetails/MenuDetails.jsx
+++ src/components/Menu/MenuDetails/MenuDetails.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MenuDetails = props => (
   <div className='details'>
     <p className='details-title'>{props.match.params.page}</p>
+    <Link to='/menu' className="details-link">Back to menu</Link>
   </div>
 );
 
